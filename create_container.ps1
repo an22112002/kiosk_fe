54cca682b4397a12dfcbe14568d933774a4e6ab6
NgoAn <blackleft9999@gmail.com>
2026-06-17 17:34:53 +0700

    ok

--- create_container.ps1
+++ create_container.ps1
@@ -12,8 +12,6 @@ if ($image) {
 Write-Host "Image '$image_name' does not exist. Continue install process."
 docker build -t $image_name .
 
-$ban_file_path = "C:\ban.txt"
-
 if ($LASTEXITCODE -ne 0) {
     Write-Host "Failed to build image '$image_name'. Stop install process."
     exit 1
@@ -21,7 +19,7 @@ if ($LASTEXITCODE -ne 0) {
 
 Write-Host "Running container '$container_name' from image '$image_name'..."
 
-docker run -d -v $ban_file_path:/usr/share/nginx/html/file/ban.txt --restart=always --name $container_name -p 80:80 $image_name
+docker run -d -v c:/ban.txt:/usr/share/nginx/html/file/ban.txt --restart=always --name $container_name -p 80:80 $image_name
 
 
 if ($LASTEXITCODE -ne 0) {
